corrected angle calculation

fiduceo/tool/radprop/algorithms/avhrr_simple_sst.py
@@ -9,8 +9,9 @@ class AvhrrSimpleSST:
         ch4_data = ch4_variable.data
         ch5_data = dataset["Ch5_Bt"].data
         sza_data = dataset["satellite_zenith_angle"].data
+        sza_data_rad = sza_data * np.pi / 180.0
 
-        sst = ch4_data + 1.0 / np.cos(sza_data) * (ch4_data - ch5_data) + 1.0
+        sst = ch4_data + 1.0 / np.cos(sza_data_rad) * (ch4_data - ch5_data) + 1.0
 
         return Variable(ch4_variable.dims, sst)
 